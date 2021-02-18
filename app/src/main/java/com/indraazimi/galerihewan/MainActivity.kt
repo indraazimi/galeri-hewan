7137ae01b1a790f6f398b166f14d78fdf390be32
Indra Azimi <indraazimi@yahoo.com>
2021-02-18 22:34:07 +0700

    Menambahkan tombol Next

--- app/src/main/java/com/indraazimi/galerihewan/MainActivity.kt
+++ app/src/main/java/com/indraazimi/galerihewan/MainActivity.kt
@@ -10,11 +10,20 @@
 package com.indraazimi.galerihewan
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nextButton: Button = findViewById(R.id.nextButton)
+        nextButton.setOnClickListener { showNext() }
+    }
+
+    private fun showNext() {
+        Log.d("MainActivity", "Tombol diklik!")
     }
 }
