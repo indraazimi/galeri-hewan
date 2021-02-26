e348c498844fc1aacf98f74d5920da2c9c19efb7
Indra Azimi <indraazimi@yahoo.com>
2021-02-26 10:38:45 +0700

    Menambahkan data dummy

--- app/src/main/java/com/indraazimi/galerihewan/MainActivity.kt
+++ app/src/main/java/com/indraazimi/galerihewan/MainActivity.kt
@@ -10,11 +10,32 @@
 package com.indraazimi.galerihewan
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("MainActivity", "Jumlah data: " + getData().size)
+    }
+
+    // Biasanya kita mengambil data dari database, atau server.
+    // Tapi karena materi belum sampai, kita buat dummy saja.
+    private fun getData(): List<Hewan> {
+        return listOf(
+                Hewan("Angsa", "Cygnus olor"),
+                Hewan("Ayam", "Gallus gallus"),
+                Hewan("Bebek", "Cairina moschata"),
+                Hewan("Domba", "Ovis ammon"),
+                Hewan("Kalkun", "Meleagris gallopavo"),
+                Hewan("Kambing", "Capricornis sumatrensis"),
+                Hewan("Kelinci", "Oryctolagus cuniculus"),
+                Hewan("Kerbau", "Bubalus bubalis"),
+                Hewan("Kuda", "Equus caballus"),
+                Hewan("Sapi", "Bos taurus"),
+        )
     }
 }
