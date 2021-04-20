2ecb6cb317351b8d26c207b3bfd9906e4137c333
Indra Azimi <indraazimi@yahoo.com>
2021-04-20 12:28:30 +0700

    Membuat view model untuk fitur main

--- app/src/main/java/com/indraazimi/galerihewan/ui/main/MainFragment.kt
+++ app/src/main/java/com/indraazimi/galerihewan/ui/main/MainFragment.kt
@@ -14,41 +14,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
-import com.indraazimi.galerihewan.R
 import com.indraazimi.galerihewan.databinding.FragmentMainBinding
-import com.indraazimi.galerihewan.model.Hewan
 
 class MainFragment : Fragment() {
 
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
     private lateinit var binding: FragmentMainBinding
+    private lateinit var myAdapter: MainAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         binding = FragmentMainBinding.inflate(layoutInflater, container, false)
+        myAdapter = MainAdapter()
         with(binding.recyclerView) {
             addItemDecoration(DividerItemDecoration(context, RecyclerView.VERTICAL))
-            adapter = MainAdapter(getData())
+            adapter = myAdapter
             setHasFixedSize(true)
         }
         return binding.root
     }
 
-    // Biasanya kita mengambil data dari database, atau server.
-    // Tapi karena materi belum sampai, kita buat dummy saja.
-    private fun getData(): List<Hewan> {
-        return listOf(
-            Hewan("Angsa", "Cygnus olor", R.drawable.angsa),
-            Hewan("Ayam", "Gallus gallus", R.drawable.ayam),
-            Hewan("Bebek", "Cairina moschata", R.drawable.bebek),
-            Hewan("Domba", "Ovis ammon", R.drawable.domba),
-            Hewan("Kalkun", "Meleagris gallopavo", R.drawable.kalkun),
-            Hewan("Kambing", "Capricornis sumatrensis", R.drawable.kambing),
-            Hewan("Kelinci", "Oryctolagus cuniculus", R.drawable.kelinci),
-            Hewan("Kerbau", "Bubalus bubalis", R.drawable.kerbau),
-            Hewan("Kuda", "Equus caballus", R.drawable.kuda),
-            Hewan("Sapi", "Bos taurus", R.drawable.sapi),
-        )
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.getData().observe(viewLifecycleOwner, {
+            myAdapter.updateData(it)
+        })
     }
 }
