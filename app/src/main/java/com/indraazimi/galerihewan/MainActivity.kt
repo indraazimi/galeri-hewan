e1b38767b7570418a8e4e618bb7dfe0c7d1d0eb6
Indra Azimi <indraazimi@yahoo.com>
2022-05-30 11:38:23 +0700

    Mempelajari daur hidup Activity

--- app/src/main/java/com/indraazimi/galerihewan/MainActivity.kt
+++ app/src/main/java/com/indraazimi/galerihewan/MainActivity.kt
@@ -10,6 +10,7 @@
 package com.indraazimi.galerihewan
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -17,5 +18,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i("MainActivity", "onCreate dipanggil.")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MainActivity", "onStart dipanggil.")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("MainActivity", "onResume dipanggil.")
+    }
+
+    override fun onPause() {
+        Log.i("MainActivity", "onPause dipanggil.")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.i("MainActivity", "onStop dipanggil.")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.i("MainActivity", "onDestroy dipanggil.")
+        super.onDestroy()
     }
 }
