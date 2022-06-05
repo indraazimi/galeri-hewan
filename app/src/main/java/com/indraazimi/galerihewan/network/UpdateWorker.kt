639a49a642cc6cb466fd6b70e78d8149f9216c64
Indra Azimi <indraazimi@yahoo.com>
2022-06-05 22:44:38 +0700

    Membuat notifikasi updater

--- app/src/main/java/com/indraazimi/galerihewan/network/UpdateWorker.kt
+++ app/src/main/java/com/indraazimi/galerihewan/network/UpdateWorker.kt
@@ -9,18 +9,45 @@
 
 package com.indraazimi.galerihewan.network
 
+import android.app.PendingIntent
 import android.content.Context
-import android.util.Log
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.indraazimi.galerihewan.MainActivity
+import com.indraazimi.galerihewan.R
 
 class UpdateWorker(
     context: Context,
     workerParams: WorkerParameters
 ) : Worker(context, workerParams) {
 
+    private val notificationId = 44
+
     override fun doWork(): Result {
-        Log.d("Worker", "Menjalankan proses background..")
+        // Update data di background dilakukan dalam 3 tahapan:
+        // 1. Request data yang baru ke server
+        // 2. Simpan di database sebagai cache
+        // 3. Beri tahu pengguna ada data baru
+
+        // Langkah 1 dan 2 sudah diajarkan di modul sebelumnya.
+        // Jadi di sini kita langsung loncat ke langkah 3 saja.
+
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val pendingIntent = PendingIntent.getActivity(applicationContext, 0, intent, 0)
+
+        val builder = NotificationCompat.Builder(applicationContext, MainActivity.CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher_round)
+            .setContentTitle(applicationContext.getString(R.string.notif_title))
+            .setContentText(applicationContext.getString(R.string.notif_text))
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+        val manager = NotificationManagerCompat.from(applicationContext)
+        manager.notify(notificationId, builder.build())
 
         return Result.success()
     }
