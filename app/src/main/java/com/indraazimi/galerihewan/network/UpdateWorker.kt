53f6f6fa4a254954bb73806401c4fb05910b04c1
poncoe <poncoe4mac@gmail.com>
2023-05-06 03:58:53 +0700

    fix error forceclose caused by notification security check exception & handle pendingintent for android 12+

--- app/src/main/java/com/indraazimi/galerihewan/network/UpdateWorker.kt
+++ app/src/main/java/com/indraazimi/galerihewan/network/UpdateWorker.kt
@@ -9,19 +9,26 @@
 
 package com.indraazimi.galerihewan.network
 
+import android.Manifest
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.indraazimi.galerihewan.MainActivity
 import com.indraazimi.galerihewan.R
 
+
 class UpdateWorker(
     context: Context,
-    workerParams: WorkerParameters
+    workerParams: WorkerParameters,
 ) : Worker(context, workerParams) {
 
     private val notificationId = 44
@@ -37,7 +44,35 @@ class UpdateWorker(
 
         val intent = Intent(applicationContext, MainActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        val pendingIntent = PendingIntent.getActivity(applicationContext, 0, intent, 0)
+
+        // mengecek PendingIntent Mutable jika berjalan pada android 12+
+        val pendingIntent = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
+            PendingIntent.getActivity(applicationContext, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE)
+        } else {
+            PendingIntent.getActivity(applicationContext, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
+        // pengecekan jika izin notifikasi di izinkan pengguna (pass exception securitycheck android 12+)
+        if (ContextCompat.checkSelfPermission(
+                applicationContext,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // jika berulangkali tidak memberikan izin akan menampilkan dialog
+            // dialog untuk mengiizinkan izin notifikasi secara manual
+            val packageName = applicationContext.packageName
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+            intent.data = Uri.parse("package:$packageName")
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            applicationContext.startActivity(intent)
+
+            // jika izin tidak diizinkan, maka akan mengembalikan result gagal
+            // dan notifikasi tidak berjalan
+            return Result.failure()
+        }
+
+        // jika izin notifikasi diterima akan melakukan tugas yang ada dibawah ini
 
         val builder = NotificationCompat.Builder(applicationContext, MainActivity.CHANNEL_ID)
             .setSmallIcon(R.mipmap.ic_launcher_round)
@@ -47,6 +82,7 @@ class UpdateWorker(
             .setContentIntent(pendingIntent)
             .setAutoCancel(true)
         val manager = NotificationManagerCompat.from(applicationContext)
+
         manager.notify(notificationId, builder.build())
 
         return Result.success()
