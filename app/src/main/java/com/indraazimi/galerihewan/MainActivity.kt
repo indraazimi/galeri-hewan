d80216978828a2435cf1ef1458f74f170824d656
poncoe <poncoe4mac@gmail.com>
2023-05-06 13:28:48 +0700

    added check premission when firstime open app

--- app/src/main/java/com/indraazimi/galerihewan/MainActivity.kt
+++ app/src/main/java/com/indraazimi/galerihewan/MainActivity.kt
@@ -9,14 +9,25 @@
 
 package com.indraazimi.galerihewan
 
+import android.app.AlertDialog
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_APP_SETTINGS = 321
+    private val PERMISSION_REQUEST_CODE = 123
 
     companion object {
         const val CHANNEL_ID = "updater"
@@ -26,14 +37,69 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        startNotification()
+    }
+
+    private fun startNotification() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS)
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            // jika sudah mendapatkan izin akan menjalankan notifikasi
+            notification()
+        } else {
+            // jika belum mendapatkan izin sama sekali, akan menampilkan dialog izin notifikasi
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.POST_NOTIFICATIONS),
+                PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
+    private fun notification() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val name = getString(R.string.channel_name)
             val importance = NotificationManager.IMPORTANCE_DEFAULT
             val channel = NotificationChannel(CHANNEL_ID, name, importance)
             channel.description = getString(R.string.channel_desc)
 
-            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val manager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             manager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun goToSettings() {
+        val myAppSettings = Intent(
+            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+            Uri.parse("package:$packageName")
+        )
+        myAppSettings.addCategory(Intent.CATEGORY_DEFAULT)
+        myAppSettings.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        @Suppress("DEPRECATION")
+        startActivityForResult(myAppSettings, REQUEST_APP_SETTINGS)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // izin diterima dan menampilkan notifikasi
+                notification()
+            } else {
+                // jika pengguna terus-terusan menolak izin, akan menampilkan dialog untuk izin secara manual
+                AlertDialog.Builder(this)
+                    .setTitle(R.string.app_name)
+                    .setMessage(R.string.premissionsNotif)
+                    .setCancelable(true)
+                    .setPositiveButton(
+                        "OK"
+                    ) { _: DialogInterface?, _: Int -> goToSettings() }
+                    .show()
+            }
         }
     }
 }
